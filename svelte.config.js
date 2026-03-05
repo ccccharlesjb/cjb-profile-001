067afbc7abaae6794abdd734438acdf78df645ce
Charles <charlesbelamide@yahoo.com>
2026-03-05 22:12:48 +0800

    chore: install and configure static adapter for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,31 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static'; // <--- Check this line!
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
-	preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+    extensions: ['.svelte', '.svx'],
+    preprocess: [
+        vitePreprocess(), 
+        mdsvex()
+    ],
+
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            precompress: false,
+            strict: false
+        }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/cjb-profile-001' : '',
+            relative: true
+        },
+prerender: {
+            handleHttpError: 'warn' // This changes "Error" to just a "Warning"
+        }
+    }
 };
 
 export default config;
